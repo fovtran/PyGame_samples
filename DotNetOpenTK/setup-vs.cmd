415cb0e3178d3e049b2a5650346356a90dc8061d
DiegoCad <diego@mercury>
2023-10-01 14:34:39 -0300

    removal dotnet opentk

--- DotNetOpenTK/setup-vs.cmd
+++
@@ -1,2 +0,0 @@
-set DOTNET_CLI_TELEMETRY_OPTOUT=1
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\vc\Auxiliary\Build\vcvarsall.bat" x64
--- DotNetOpenTK/src2/build.bat
+++
@@ -1,5 +0,0 @@
-# Build advanced http server
-
-rem csc /r:System.Net.dll /t:library SimpleWebServer.cs
-rem csc /r:System.Net.dll /r:NVelocity.dll /t:library VelocityTemplateRenderer.cs
-rem csc /r:System.Net.dll,VelocityTemplateRenderer.dll,SimpleWebServer.dll Server.cs
